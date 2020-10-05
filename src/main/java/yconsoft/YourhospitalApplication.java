f85464b9dfd47736ccbb29f39e9cd6a2b18966c3
ephraimd <olagokedammy@gmail.com>
2020-10-05 12:24:48 +0100

    Add footnote comments

--- src/main/java/yconsoft/YourhospitalApplication.java
+++ src/main/java/yconsoft/YourhospitalApplication.java
@@ -3,12 +3,19 @@ package yconsoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Your Hopsital Software, built with Love by Ephraim (^_^)
+ */
 @SpringBootApplication
 public class YourhospitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YourhospitalApplication.class, args);
 		System.out.println("Server started successfully!");
+		//we see Jesus, made a little lower than THe Angels, so we can know that God is Loving and Humble
+		//so that they that believe on Him, He will change and give Joy and Hope
+		//far beyond what life can offer.
+		//John 3:16 Notes!
 	}
 
 }
