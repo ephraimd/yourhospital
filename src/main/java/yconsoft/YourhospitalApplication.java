22282e1dfd5868eb7807d3ac271d81c0ce2db37f
ephraimd <olagokedammy@gmail.com>
2020-10-05 12:14:23 +0100

    First Stable commit

---
+++ src/main/java/yconsoft/YourhospitalApplication.java
@@ -0,0 +1,14 @@
+package yconsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourhospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourhospitalApplication.class, args);
+		System.out.println("Server started successfully!");
+	}
+
+}
